fee3937d361ec9969f0b317157b80aaae1ef693b
mohaimen.khan@mail.utoronto.ca <mohaimen.khan@mail.utoronto.ca>
2019-11-05 09:30:05 -0500

    Changed application controller to take in authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_152209) do
+ActiveRecord::Schema.define(version: 2019_11_04_123930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,8 +61,8 @@ ActiveRecord::Schema.define(version: 2019_10_29_152209) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "fname"
     t.string "lname"
-    t.boolean "admin", default: false
     t.string "payment_card"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
