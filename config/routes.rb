d4678a62ea663b487e0a041eb319bcbe6268f0b7
Zihan Zhao <zzh@ZHAOSMBP13.local>
2019-11-19 10:43:34 -0500

    Merging routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', :to => 'about#index'
+  get 'home', :to => 'home#index'
 end
