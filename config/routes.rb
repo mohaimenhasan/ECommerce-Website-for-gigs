1273a70342ae8439952d6fc16c1e4c178948e4dc
Kevin Yang <yanghykevin@outlook.com>
2019-11-19 10:20:01 -0500

    Search functionality
    Added the search bar and basic functionality. (Need to modify the redirect path to jobs)

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     resources :jobs
     resources :wallets
   end
+
+  get '/search' => 'jobs#search', :as => 'search_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
