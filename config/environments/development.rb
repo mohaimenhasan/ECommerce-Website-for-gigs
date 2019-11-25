09512789c56d441472d3b7c85adc7e92753ead8d
Hongyi-Yang <56452856+Hongyi-Yang@users.noreply.github.com>
2019-11-24 21:49:14 -0500

    Added basic setting for Action Mailer
    modify env file and create a mailer for sending emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -54,6 +54,15 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            'coolgigs444@gmail.com',
+    password:             'hello123ece444',
+    authentication:       'plain',
+    enable_starttls_auto: true }
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
