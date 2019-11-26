7a1313461f5a1eaa300cb55aa2c20b68c71ca1b2
jhunnie <jenniej.hu@mail.utoronto.ca>
2019-11-26 16:06:22 -0500

    added files for static pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_173606) do
+ActiveRecord::Schema.define(version: 2019_11_25_193450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2019_11_25_173606) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "receiver_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id", "receiver_id"], name: "index_conversations_on_author_id_and_receiver_id", unique: true
+    t.index ["author_id"], name: "index_conversations_on_author_id"
+    t.index ["receiver_id"], name: "index_conversations_on_receiver_id"
   end
 
   create_table "jobs", force: :cascade do |t|
@@ -42,6 +52,16 @@ ActiveRecord::Schema.define(version: 2019_11_25_173606) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["job_id"], name: "index_ongoing_tasks_on_job_id"
     t.index ["user_id"], name: "index_ongoing_tasks_on_user_id"
+  end
+
+  create_table "personal_messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_personal_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_personal_messages_on_user_id"
   end
 
   create_table "subcategories", force: :cascade do |t|
@@ -76,4 +96,6 @@ ActiveRecord::Schema.define(version: 2019_11_25_173606) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "personal_messages", "conversations"
+  add_foreign_key "personal_messages", "users"
 end
