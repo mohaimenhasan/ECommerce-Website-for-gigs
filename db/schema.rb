3b68d69e0b3f385317b98dd18c0d410e2296eaeb
aaron-zhao-ut <zzh.zhao@mail.utoronoto.ca>
2019-11-26 15:37:07 -0500

    Redone about page so that it lays out with the html currently in changes

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -77,3 +78,84 @@ ActiveRecord::Schema.define(version: 2019_11_25_173606) do
   end
 
 end
+=======
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_11_25_173606) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.string "subcategory_id"
+    t.float "cost"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_id"
+    t.index ["user_id"], name: "index_jobs_on_user_id"
+  end
+
+  create_table "ongoing_tasks", force: :cascade do |t|
+    t.bigint "job_id"
+    t.bigint "user_id"
+    t.string "status"
+    t.float "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["job_id"], name: "index_ongoing_tasks_on_job_id"
+    t.index ["user_id"], name: "index_ongoing_tasks_on_user_id"
+  end
+
+  create_table "subcategories", force: :cascade do |t|
+    t.bigint "category_id"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "fname"
+    t.string "lname"
+    t.boolean "admin", default: false
+    t.string "stripe_id"
+    t.string "street"
+    t.string "city"
+    t.string "province"
+    t.string "postalCode"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
+>>>>>>> Stashed changes
