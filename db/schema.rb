addb027d02440c77dd74a424976292c88571d19c
Hongyi-Yang <56452856+Hongyi-Yang@users.noreply.github.com>
2019-11-26 16:16:15 -0500

    Changed Gemfile lock with ruby vers

--- db/schema.rb
+++ db/schema.rb
@@ -84,14 +84,14 @@ ActiveRecord::Schema.define(version: 2019_11_25_193450) do
     t.string "fname"
     t.string "lname"
     t.boolean "admin", default: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.string "stripe_id"
     t.string "street"
     t.string "city"
     t.string "province"
     t.string "postalCode"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
