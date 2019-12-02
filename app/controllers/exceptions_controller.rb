0a1931e7482898fe9c38eafc392296b00b738df5
unknown <waseem.qadeer@mail.utoronto.ca>
2019-12-02 11:48:47 -0500

    Added the error controllers and views

---
+++ app/controllers/exceptions_controller.rb
@@ -0,0 +1,50 @@
+class ExceptionsController < ApplicationController::Base
+
+       #Response
+       respond_to :html, :xml, :json
+
+       #Details
+       before_action :status
+
+       #Layout
+       layout :layout
+
+       #######################
+
+       #Show
+       def show
+             respond_with status: @status
+       end
+
+       #######################
+
+      protected
+
+      #Info
+      def status
+           @exception  = env['action_dispatch.exception']
+           @status     = ActionDispatch::ExceptionWrapper.new(env, @exception).status_code
+           @response   = ActionDispatch::ExceptionWrapper.rescue_responses[@exception.class.name]
+     end
+
+     #Format
+    def details
+      @details ||= {}.tap do |h|
+         I18n.with_options scope: [:exception, :show, @response], exception_name: @exception.class.name, exception_message: @exception.message do |i18n|
+                 h[:name]    = i18n.t "#{@exception.class.name.underscore}.title", default: i18n.t(:title, default: @exception.class.name)
+                 h[:message] = i18n.t "#{@exception.class.name.underscore}.description", default: i18n.t(:description, default: @exception.message)
+             end
+        end
+    end
+    helper_method :details
+  
+       #######################
+
+       private
+
+       #Layout
+       def layout_status
+            @status.to_s == "404" ? "application" : "error"
+       end
+
+end
