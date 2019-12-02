2fb1079af82ae5ca74a8470d31452283eec552d8
mohaimen.khan@mail.utoronto.ca <mohaimen.khan@mail.utoronto.ca>
2019-12-02 16:16:42 -0500

    Getting rid of Jenny's code for Heroku for mailer

--- app/mailers/contact_mailer.rb
+++
@@ -1,8 +0,0 @@
-class ContactsMailer < ActionMailer::Base
-    default from: "admin@example.com"
-    
-    def general_message(contact)
-        @contact = contact
-        mail( :to => "admin@example.com", :subject => "You Have a Message From Your Website")
-    end
-  end
