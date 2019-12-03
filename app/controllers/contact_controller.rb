40ab7c256a2dc4648fe6a3d1a0569befa1f926dd
jhunnie <jenniej.hu@mail.utoronto.ca>
2019-12-03 03:20:38 -0500

    small html changes

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,22 +2,4 @@ class ContactController < ApplicationController
     def index
         @user = current_user
     end 
-
-    def new
-        @contact = Contact.new
-    end
-    
-    def create
-        @contact = Contact.new(params[:contact])
-        
-        if @contact.save
-            ContactsMailer.general_message(@contact).deliver
-            render :thanks
-        else
-            render :new
-        end
-    end
-      
-    def thanks
-    end
 end
