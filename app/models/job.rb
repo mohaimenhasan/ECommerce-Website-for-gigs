132f0159184fd03080218c4a874f681157b5facc
unknown <waseem.qadeer@mail.utoronto.ca>
2019-12-02 22:06:08 -0500

    Added the validations for the Job creation. Errors are shown to the users if any field is left empty.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,11 @@
 class Job < ApplicationRecord
   belongs_to :user
   belongs_to :subcategory
+
+  validates :name, presence: true,
+                    length: { minimum: 5 }
+  validates :subcategory_id, presence: true
+  validates :cost, presence: true
+  validates :description, presence: true,
+                    length: { minimum: 5 }
 end
