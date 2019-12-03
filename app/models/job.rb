381f7a3f580ff5af5ddbf9877ee02de97a237c53
unknown <waseem.qadeer@mail.utoronto.ca>
2019-12-02 22:18:54 -0500

    Added some Unit Tests for Job creation to ensure jobs are being created properly with all the required information.

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,10 +2,8 @@ class Job < ApplicationRecord
   belongs_to :user
   belongs_to :subcategory
 
-  validates :name, presence: true,
-                    length: { minimum: 5 }
+  validates :name, presence: true
   validates :subcategory_id, presence: true
   validates :cost, presence: true
-  validates :description, presence: true,
-                    length: { minimum: 5 }
+  validates :description, presence: true
 end
